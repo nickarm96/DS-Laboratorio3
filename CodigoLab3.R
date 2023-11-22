ecc9836d4221fe08ea1e9431fb9b72fb58f34c46
Augurio Armando Rosales Alarcon <auguriora@TTC.CAT>
2023-11-22 16:35:38 +0100

    Send5

---
+++ CodigoLab3.R
@@ -0,0 +1,30 @@
+# load libraries
+library(readr)
+library(tidyr)
+library(dplyr)
+library(data.table)
+library(mltools)
+library(ggplot2)
+
+#("ORIGEN","FECHA","TIPO","URL","PROTOCOLO","PETICIONES","BYTES")
+
+# convertir campos a factor
+
+  
+  epa_http_one_hot <- epa_http %>% mutate(
+    TIPO = as.factor(TIPO), 
+    PROTOCOLO = as.factor(PROTOCOLO),
+    PETICIONES = as.factor(PETICIONES),
+    BYTES = as.factor(BYTES)
+    )
+  
+# Eliminar tablas no numericas
+  epa_http_one_hot$ORIGEN <- NULL
+  epa_http_one_hot$FECHA <- NULL
+  epa_http_one_hot$URL <- NULL
+
+  print(lapply(epa_http_one_hot, class))
+  
+  epa_http_one_hot <-select(epa_http, TIPO, PROTOCOLO, PETICIONES, BYTES)
+
+epa_http_one_hot <- one_hot(as.data.table(epa_http), sparsifyNAs = TRUE)

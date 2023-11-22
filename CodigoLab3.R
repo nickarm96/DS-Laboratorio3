833892372f6715ec64713f73e3db577fdfda19de
Augurio Armando Rosales Alarcon <auguriora@TTC.CAT>
2023-11-22 17:09:20 +0100

    send6

--- CodigoLab3.R
+++ CodigoLab3.R
@@ -11,20 +11,20 @@ library(ggplot2)
 # convertir campos a factor
 
   
-  epa_http_one_hot <- epa_http %>% mutate(
-    TIPO = as.factor(TIPO), 
-    PROTOCOLO = as.factor(PROTOCOLO),
-    PETICIONES = as.factor(PETICIONES),
-    BYTES = as.factor(BYTES)
-    )
+epa_http_one_hot <- epa_http %>% mutate(
+  TIPO = as.factor(TIPO), 
+  PROTOCOLO = as.factor(PROTOCOLO),
+  PETICIONES = as.factor(PETICIONES),
+  BYTES = as.factor(BYTES)
+  )
   
 # Eliminar tablas no numericas
-  epa_http_one_hot$ORIGEN <- NULL
-  epa_http_one_hot$FECHA <- NULL
-  epa_http_one_hot$URL <- NULL
+epa_http_one_hot$ORIGEN <- NULL
+epa_http_one_hot$FECHA <- NULL
+epa_http_one_hot$URL <- NULL
 
-  print(lapply(epa_http_one_hot, class))
+print(lapply(epa_http_one_hot, class))
   
-  epa_http_one_hot <-select(epa_http, TIPO, PROTOCOLO, PETICIONES, BYTES)
+epa_http_one_hot <-select(epa_http, TIPO, PROTOCOLO, PETICIONES, BYTES)
 
 epa_http_one_hot <- one_hot(as.data.table(epa_http), sparsifyNAs = TRUE)
